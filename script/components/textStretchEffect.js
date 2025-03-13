75bd39e46db1a17996218128691bc35e34b9b277
kim ye chan <ycraah@gmail.com>
2025-03-13 21:21:39 +0900

    텍스트 scale 효과를 구현

--- script/components/textStretchEffect.js
+++ script/components/textStretchEffect.js
@@ -1,5 +1,33 @@
 const textStretchEffect = () =>{
-    
+    const svgTextArr = ["#753db5", "#73d2b7", "#eb7fb0", "#ef20a1", "#e3fe59", "#4acce7", "#f3dc91", "#ea89b7", "#63d2c5", "#8c29b6", "#ef37a6", "#e4ff5b", "#56d6d6", "#f220a2", "#813abc", "#e3ff5a"]; 
+    svgTextArr.reverse();
+    const pathsArr = document.querySelectorAll('#web > path');
+    pathsArr.forEach((path, index) => {
+        path.style.fill = svgTextArr[index];
+        path.style.transition = "all 0.2s";
+        path.style.transformOrigin = "50% 50%";
+        
+        path.addEventListener("mouseenter", () => {
+            path.style.transform = "scaleY(1.2)";
+            if (path.previousElementSibling) {
+                path.previousElementSibling.style.transform = "scaleY(1.1)";
+            }
+            if (path.nextElementSibling) {
+                path.nextElementSibling.style.transform = "scaleY(1.1)";
+            }
+          });
+          path.addEventListener("mouseleave", () => {
+            path.style.transform = "scaleY(1)";
+            if (path.previousElementSibling) {
+                path.previousElementSibling.style.transform = "scaleY(1)";
+            }
+            if (path.nextElementSibling) {
+                path.nextElementSibling.style.transform = "scaleY(1)";
+            }
+          });
+        
+    }); 
+
 }
 
 export {textStretchEffect}
