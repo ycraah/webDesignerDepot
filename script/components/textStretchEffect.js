d731a32b199f4b2389e0802aaba30ff2480599f4
kim ye chan <ycraah@gmail.com>
2025-03-13 20:00:24 +0900

    섹션 글자 늘어나는 효과를 위해 svg 파일 html입력

---
+++ script/components/textStretchEffect.js
@@ -0,0 +1,5 @@
+const textStretchEffect = () =>{
+    
+}
+
+export {textStretchEffect}
--- script/main.js
+++ script/main.js
@@ -1,7 +1,9 @@
 import { ChangeLogoColorOnHover } from './components/logoEffect.js';
 import {textUnderlineEffect} from './components/underline.js';
 import { searchBarStretchLeft } from './components/search.js';
+import { textStretchEffect } from './components/textStretchEffect.js';
 
 ChangeLogoColorOnHover();
 textUnderlineEffect();
 searchBarStretchLeft();
+textStretchEffect();
