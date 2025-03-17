4fcab3fd08ae1aae89e3448d8c4596772e62b2d0
kim ye chan <ycraah@gmail.com>
2025-03-17 18:27:17 +0900

    재시도

--- script/components/contentBox.js
+++ script/components/contentBox.js
@@ -1,12 +1,12 @@
 const contentBoxGenerator = () => {
     const contentBoxs = document.querySelectorAll(".content-box");
     contentBoxs.forEach((contentBox) => {
-        const newDiv = document.createElement('div');
-        newDiv.classList.add('border--rainbow-rotate');
-        const newDivChild = document.createElement('div');
-        newDivChild.classList.add('inner-box');
-        newDiv.appendChild(newDivChild);
-        contentBox.appendChild(newDiv);
+        const $newDiv = document.createElement('div');
+        $newDiv.classList.add('border--rainbow-rotate');
+        const $newDivChild = document.createElement('div');
+        $newDivChild.classList.add('inner-box');
+        $newDiv.appendChild($newDivChild);
+        contentBox.appendChild($newDiv);
         contentBox.addEventListener("mouseenter", ()=> {
             const borderBox = contentBox.querySelector('.border--rainbow-rotate');
             const imgBox = contentBox.querySelector('.content-box__image');
