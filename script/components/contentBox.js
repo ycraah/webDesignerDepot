c9752b81db4f2819bb45cef2bed8cd5a5bb5d231
kim ye chan <ycraah@gmail.com>
2025-03-17 23:38:11 +0900

    화살표 색 반전 효과

--- script/components/contentBox.js
+++ script/components/contentBox.js
@@ -10,14 +10,18 @@ const contentBoxGenerator = () => {
         contentBox.addEventListener("mouseenter", ()=> {
             const borderBox = contentBox.querySelector('.border--rainbow-rotate');
             const imgBox = contentBox.querySelector('.content-box__image');
+            const icon = contentBox.querySelector('.content-box__icon');
             borderBox.classList.add('active');
             imgBox.classList.add('active');
+            icon.classList.add('active');
         });
         contentBox.addEventListener("mouseleave", () => {
             const borderBox = contentBox.querySelector('.border--rainbow-rotate');
             const imgBox = contentBox.querySelector('.content-box__image');
+            const icon = contentBox.querySelector('.content-box__icon');
             borderBox.classList.remove('active');
             imgBox.classList.remove('active');
+            icon.classList.remove('active');
         });
     });
 }
