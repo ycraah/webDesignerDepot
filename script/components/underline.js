11c0f9e95553ece1dd2797806c4ad92fab8d3f83
kim ye chan <ycraah@gmail.com>
2025-03-19 02:47:19 +0900

    GitPage 오류로 인해서 svg를 직접 때려박음

--- script/components/underline.js
+++ script/components/underline.js
@@ -1,7 +1,7 @@
 const textUnderlineEffect = (() => {
     const navLinks = document.querySelectorAll(".menu__link");
     navLinks.forEach(link => {
-        link.insertAdjacentHTML("afterend", "<div class='underline disabled'></div>");
+        link.insertAdjacentHTML("afterend", "<div class='underline disabled'><svg id='wavy-svg' xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' xmlns:ev='http://www.w3.org/2001/xml-events' viewBox='0 0 20 4'><style type='text/css'>.st0{animation:shift 0.4s linear infinite;}@keyframes shift {from {transform:translateX(0);}to {transform:translateX(-20px);}}</style><path fill='none' stroke='#E3FF59' stroke-width='1' class='st0' d='M0,3.5 c 5,0,5,-3,10,-3 s 5,3,10,3 c 5,0,5,-3,10,-3 s 5,3,10,3'/></svg></div>");
     }); //underline 생성기
 
     for(let i = 0; i < navLinks.length; i++){
