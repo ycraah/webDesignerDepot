3ad473128591726d3dc6ffade4aaecf09129208f
kim ye chan <ycraah@gmail.com>
2025-03-20 11:39:07 +0900

    item 페이지 생성- 기본 설정

---
+++ script/item.js
@@ -0,0 +1,14 @@
+import { ChangeLogoColorOnHover } from './components/logoEffect.js';
+import {textUnderlineEffect} from './components/underline.js';
+import { searchBarStretchLeft } from './components/search.js';
+import { textStretchEffect } from './components/textStretchEffect.js';
+import {contentBoxGenerator, contentBoxCircleAfterData} from './components/contentBox.js';
+import {responseMenu} from './components/responseMenu.js';
+
+ChangeLogoColorOnHover();
+textUnderlineEffect();
+searchBarStretchLeft();
+textStretchEffect();
+contentBoxGenerator();
+contentBoxCircleAfterData();
+responseMenu();
